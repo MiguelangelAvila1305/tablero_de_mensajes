bec384b30c43960a7474f350dd2601448e967e3d
Miguelangel Avila <miguelangel.a1305@gmail.com>
2023-04-01 16:36:04 -0400

    Message board 1

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 # Por cada cosa que hagamos en models tenemos que hacer una migracion 
 # python manage.py makemigrations 
+# DJANGO utiliza un orm por lo que solo tenemos que utilizar clases para construir tablas
+# post hereda los superpoderes de models 
 
 class Post(models.Model):
     text = models.TextField()
